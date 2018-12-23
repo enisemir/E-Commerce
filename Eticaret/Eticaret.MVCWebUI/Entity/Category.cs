cc93de3151d5316fa028ce897f782a5357e6ac89
Enis  Emir <enissemir@gmail.com>
2018-12-23 18:12:59 +0300

    Category Controller with Crud

--- Eticaret/Eticaret.MVCWebUI/Entity/Category.cs
+++ Eticaret/Eticaret.MVCWebUI/Entity/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,10 @@ namespace Eticaret.MVCWebUI.Entity
     {
         public int Id { get; set; }
 
+        [DisplayName("Kategori Adı")]
         public string Name { get; set; }
+
+        [DisplayName("Açıklama")]
         public string Description { get; set; }
 
         public List<Product> Products { get; set; }
