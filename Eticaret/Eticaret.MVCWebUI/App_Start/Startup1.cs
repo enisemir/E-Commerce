6171e1c92b55c8b9fa8e79f2d94500e3185e2e2c
Enis  Emir <enissemir@gmail.com>
2018-12-24 23:47:53 +0300

    Asp Identity İnitial Settings

---
+++ Eticaret/Eticaret.MVCWebUI/App_Start/Startup1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(Eticaret.MVCWebUI.App_Start.Startup1))]
+
+namespace Eticaret.MVCWebUI.App_Start
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Account/Login")
+
+            });
+        }
+    }
+}
