03c0932ff748826f634afaaeb23e1be6094abb5b
Enis  Emir <enissemir@gmail.com>
2018-12-27 23:54:55 +0300

    Card Model and Controller Have Created

---
+++ Eticaret/Eticaret.MVCWebUI/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using Eticaret.MVCWebUI.Entity;
+using Eticaret.MVCWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eticaret.MVCWebUI.Controllers
+{        
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddToCart(int id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == id);
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveToCart(int id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == id);
+            if (product != null)
+            {
+                GetCart().AddProduct(product, 1);
+            }
+            return RedirectToAction("Index");
+        }
+        public Cart GetCart()
+        {
+            var cart = (Cart)Session["Cart"];
+            if (cart==null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+    }
+}
---
+++ Eticaret/Eticaret.MVCWebUI/Models/Cart.cs
@@ -0,0 +1,53 @@
+﻿using Eticaret.MVCWebUI.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eticaret.MVCWebUI.Models
+{
+    public class Cart
+    {
+        private List<CartLine> _cardlines = new List<CartLine>();
+        public List<CartLine> Cardlines
+        {
+            get
+            {
+                return _cardlines;
+            }
+        }
+        public void AddProduct(Product product, int quantity)
+        {
+            var line = _cardlines.FirstOrDefault(i => i.Product.Id == product.Id);
+            if (line==null)
+            {
+                _cardlines.Add(new CartLine() { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _cardlines.RemoveAll(i => i.Product.Id == product.Id);
+        }
+
+        public Double Total()
+        {
+            return _cardlines.Sum(i => i.Product.Price * i.Quantity);
+        }
+
+        public void Clear()
+        {
+            _cardlines.Clear();
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
