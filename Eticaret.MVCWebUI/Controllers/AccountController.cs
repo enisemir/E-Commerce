794d9bbee0b32f186d6ac177b68672ae081f5b71
Enis  Emir <enissemir@gmail.com>
2018-12-30 13:26:02 +0300

    OrderDetails Controller and Model

--- Eticaret.MVCWebUI/Controllers/AccountController.cs
+++ Eticaret.MVCWebUI/Controllers/AccountController.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static Eticaret.MVCWebUI.Models.OrderDetailsModel;
 
 namespace Eticaret.MVCWebUI.Controllers
 {
@@ -41,6 +42,36 @@ namespace Eticaret.MVCWebUI.Controllers
                     OrderState=i.OrderState                                        
                 }).OrderByDescending(i=>i.OrderDate).ToList();
             return View(orders);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var entity = db.Orders
+                .Where(i => i.Id == id)
+                .Select(i => new OrderDetailsModel()
+                {
+                    OrderId = i.Id,
+                    OrderNumber = i.OrderNumber,
+                    Total = i.Total,
+                    OrderState = i.OrderState,
+                    OrderDate = i.OrderDate,
+                    AdresBasligi = i.AdresBasligi,
+                    Adres = i.Adres,
+                    Sehir = i.Sehir,
+                    Semt = i.Semt,
+                    Mahalle = i.Mahalle,
+                    PostaKodu = i.PostaKodu,
+                    OrderLines = i.OrderLines
+                    .Select(a => new OrderLineModel()
+                    {
+                        ProductId=a.ProductId,
+                        ProductName=a.Product.Name,
+                        Quantity=a.Quantity,
+                        Image=a.Product.Image,
+                        Price=a.Price
+                    }).ToList()
+                }).FirstOrDefault();
+            return View(entity);
         }
         // GET: Account
         public ActionResult Register()
---
+++ Eticaret.MVCWebUI/Models/OrderDetailsModel.cs
@@ -0,0 +1,36 @@
+﻿using Eticaret.MVCWebUI.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eticaret.MVCWebUI.Models
+{
+    public class OrderDetailsModel
+    {
+        public int OrderId { get; set; }
+        public string OrderNumber { get; set; }
+        public double Total { get; set; }
+        public DateTime OrderDate { get; set; }
+        public EnumOrderState OrderState { get; set; }
+
+        public string Username { get; set; }
+        public string AdresBasligi { get; set; }
+        public string Adres { get; set; }
+        public string Sehir { get; set; }
+        public string Semt { get; set; }
+        public string Mahalle { get; set; }
+        public string PostaKodu { get; set; }
+
+        public virtual List<OrderLineModel> OrderLines { get; set; }
+
+        public class OrderLineModel
+        {
+            public int ProductId { get; set; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            public string Image { get; set; }
+            public double Price { get; set; }
+        }
+    }
+}
