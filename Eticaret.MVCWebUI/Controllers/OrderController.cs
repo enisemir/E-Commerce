3dba7765bfc2e16ef901a6951e76843125e95da7
Enis  Emir <enissemir@gmail.com>
2018-12-30 23:19:16 +0300

    AdminOrderModel

---
+++ Eticaret.MVCWebUI/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Eticaret.MVCWebUI.Entity;
+using Eticaret.MVCWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eticaret.MVCWebUI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class OrderController : Controller
+    {
+        DataContext db = new DataContext();
+        // GET: Order
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Select(i=>new AdminOrderModel()
+            {
+                Id=i.Id,
+                OrderNumber=i.OrderNumber,
+                OrderDate=i.OrderDate,
+                OrderState=i.OrderState,
+                Total=i.Total
+            }).ToList();
+            return View(orders);
+        }
+    }
+}
--- Eticaret.MVCWebUI/Controllers/ProductController.cs
+++ Eticaret.MVCWebUI/Controllers/ProductController.cs
@@ -10,6 +10,7 @@ using Eticaret.MVCWebUI.Entity;
 
 namespace Eticaret.MVCWebUI.Controllers
 {
+    [Authorize(Roles = "admin")]
     public class ProductController : Controller
     {
         private DataContext db = new DataContext();
